0b725a4601164562cb092fe7dd5631f38532eec1
nikolicmarina415 <[nikolicmarina415@gmail.com]>
2024-06-17 19:34:07 +0200

    Dzaboravila zadnju, pa je sada dodala

--- src/main/java/com/logate/summer/Repositories/IProductRepository.java
+++ src/main/java/com/logate/summer/Repositories/IProductRepository.java
@@ -1,10 +1,13 @@
 package com.logate.summer.Repositories;
+import com.logate.summer.dto.ProductDTO;
 import com.logate.summer.entities.Product;
 import com.logate.summer.projection.ProductCustomProjection;
 import com.logate.summer.projection.ProductProjection;
 import com.logate.summer.records.ProductRecord;
+import jakarta.persistence.Tuple;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -27,37 +30,65 @@ public interface IProductRepository extends JpaRepository<Product, Integer> {
     //JPQL
 
     //select * from product join category on product.category_id = category.id where price between ?1 and ?2 and category.name = ?3
-    @Query("SELECT p FROM Product p JOIN p.category c WHERE p.price BETWEEN ?1 AND ?2 AND c.name = ?3")
-    List<Product> findByPriceBetweenAndCategoryName1(Double minPrice, Double maxPrice, String categoryName);
+    @Query(value = """
+            select product from Product product
+            join fetch Category category
+            where product.price between :startPrice and :endPrice
+            and category.name = :categoryName
+            """)
+    List<Product> findByPriceScaleAndCategoryName(@Param("startPrice") Double startPrice, @Param("endPrice") Double endPrice,
+                                                  @Param("categoryName") String name);
 
     //select product.name, product.short_description, product.price from product
     // join category on product.category_id = category.id where price between ?1 and ?2
     // and category.name = ?3 (uz pomoc constructor projection/DTO)
-    @Query("SELECT p FROM Product p JOIN p.category c " +
-            "WHERE p.price BETWEEN :minPrice AND :maxPrice AND c.name = :categoryName")
-    List<Product> findProductsByPriceRangeAndCategoryName2(Double minPrice, Double maxPrice, String categoryName);
+    @Query(value = """
+            select new com.logate.summer.dto.ProductDTO(product.name, product.shortDesc, product.price)
+            from Product product
+            join fetch Category category
+            where product.price between :startPrice and :endPrice
+            and category.name = :categoryName
+            """)
+    List<ProductDTO> findByPriceScaleAndCategoryNameDTO(@Param("startPrice") Integer startPrice, @Param("endPrice") Integer endPrice,
+                                                        @Param("categoryName") String name);
+
 
     //select product.name, product.short_description, product.price from product join category
     // on product.category_id = category.id where price between ?1 and ?2 and category.name = ?3
     // (uz pomoc tuple projection)
-    @Query("SELECT p.name AS name, p.shortDescription AS shortDescription, p.price AS price " +
-            "FROM Product p JOIN p.category c " +
-            "WHERE p.price BETWEEN ?1 AND ?2 AND c.name = ?3")
-    List<ProductProjection> findProductsByPriceRangeAndCategoryName3(Double minPrice, Double maxPrice, String categoryName);
+    @Query(value = """
+            select product.name name, product.shortDesc shortDesc, product.price price
+            from Product product
+            join fetch Category category
+            where product.price between :startPrice and :endPrice
+            and category.name = :categoryName
+            """)
+    List<Tuple> findByPriceScaleAndCategoryNameTuple(@Param("startPrice") Integer startPrice, @Param("endPrice") Integer endPrice,
+                                                     @Param("categoryName") String name);
 
     //select product.name, product.short_description, product.price from product join category
     // on product.category_id = category.id where price between ?1 and ?2 and category.name = ?3
     // (uz pomoc custom projection)
-    @Query("SELECT p.name AS name, p.shortDescription AS shortDescription, p.price AS price " +
-            "FROM Product p JOIN p.category c " +
-            "WHERE p.price BETWEEN ?1 AND ?2 AND c.name = ?3")
-    List<ProductCustomProjection> findProductsByPriceRangeAndCategoryName4(Double minPrice, Double maxPrice, String categoryName);
+    @Query(value = """
+            select product.name name, product.shortDesc shortDesc, product.price price
+            from Product product
+            join fetch Category category
+            where product.price between :startPrice and :endPrice
+            and category.name = :categoryName
+            """)
+    List<ProductProjection> findByPriceScaleAndCategoryNameCustom(@Param("startPrice") Integer startPrice, @Param("endPrice") Integer endPrice,
+                                                                  @Param("categoryName") String name);
 
     //select product.name, product.short_description, product.price from product join category
     // on product.category_id = category.id where price between ?1 and ?2 and category.name = ?3
     // (uz pomoc records)
-    @Query("SELECT NEW com.example.projections.ProductRecordProjection(p.name, p.shortDescription, p.price) " +
-            "FROM Product p JOIN p.category c " +
-            "WHERE p.price BETWEEN ?1 AND ?2 AND c.name = ?3")
-    List<ProductRecord> findProductsByPriceRangeAndCategoryName5(Double minPrice, Double maxPrice, String categoryName);
+    @Query(value = """
+            select new com.logate.summer.records.ProductDTO(product.name, product.shortDesc, product.price)
+            from Product product
+            join fetch Category category
+            where product.price between :startPrice and :endPrice
+            and category.name = :categoryName
+            """)
+    List<ProductProjection> findByPriceScaleAndCategoryNameRecords(@Param("startPrice") Integer startPrice, @Param("endPrice") Integer endPrice,
+                                                                   @Param("categoryName") String name);
 }
